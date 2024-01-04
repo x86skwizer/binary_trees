8dccfb5f1771ac1ea80b2b09b7db1fffa90c9bac
x86skwizer <yassineamrire@protonmail.com>
2024-01-04 02:02:21 -0800

    100-binary_trees_ancestor.c

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,7 +7,8 @@
  *
  * Return: Pointer to the lowest common ancestor node of the two given nodes
  */
-binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second)
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+									 const binary_tree_t *second)
 {
 	binary_tree_t *tmp1, *tmp2;
 
